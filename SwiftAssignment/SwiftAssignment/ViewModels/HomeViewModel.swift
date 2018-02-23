049c7aa9245677a6b3f6566534be36b5e3952d1d
safiyanZulfiqarTintash <safiyan.zulfiqar@tintash.com>
2018-02-23 16:11:17 +0500

    view model

---
+++ SwiftAssignment/SwiftAssignment/ViewModels/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  SwiftAssignment
+//
+//  Created by Safiyan Zulfiqar on 23/02/2018.
+//  Copyright © 2018 Safiyan Zulfiqar. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    
+}
